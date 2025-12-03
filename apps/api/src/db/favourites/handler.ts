9ca8f786d28b828061377180756be0da6e8084c0
Lasse Rasmussen <lass531n@stud.kea.dk>
2025-12-03 16:11:59 +0100

    fixed some issues in the handler

--- apps/api/src/db/favourites/handler.ts
+++ apps/api/src/db/favourites/handler.ts
@@ -1,28 +1,29 @@
-import { connectDB, TDB } from "$db/index";
+import { getDB, TDB } from "$db/index";
 import { IFavourite, INewFavourite } from "./types";
 import { favouritesTable } from "./schema";
+import { and, eq } from "drizzle-orm";
 
 class FavouritesHandler {
   #client: TDB;
 
   constructor(dbUrl: string) {
-    const db = connectDB(dbUrl);
+    const db = getDB(dbUrl);
     this.#client = db;
   }
 
   async getAllByUser(userId: number): Promise<IFavourite[]> {
-    return await this.#client.query.favourites.findMany({
-      where: (favs: typeof favouritesTable, { eq }: any) => eq(favs.userId, userId),
+    return await this.#client.query.favouritesTable.findMany({
+      where: (fields, { eq }) => eq(fields.userId, String(userId)),
     });
   }
 
   async find(query: Partial<IFavourite>): Promise<IFavourite[]> {
-    return await this.#client.query.favourites.findMany({
-      where: (favs: typeof favouritesTable, { eq, and }: any) => {
+    return await this.#client.query.favouritesTable.findMany({
+      where: (fields, { eq, and }) => {
         const clauses: any[] = [];
-        if (query.id !== undefined) clauses.push(eq(favs.id, query.id));
-        if (query.userId !== undefined) clauses.push(eq(favs.userId, query.userId));
-        if (query.pizzaId !== undefined) clauses.push(eq(favs.pizzaId, query.pizzaId));
+        if (query.id !== undefined) clauses.push(eq(fields.id, query.id));
+        if (query.userId !== undefined) clauses.push(eq(fields.userId, String(query.userId)));
+        if (query.pizzaId !== undefined) clauses.push(eq(fields.pizzaId, query.pizzaId));
         if (clauses.length === 0) return undefined;
         return clauses.length === 1 ? clauses[0] : and(...clauses);
       },
@@ -31,17 +32,20 @@ class FavouritesHandler {
 
   async addFavourite(input: INewFavourite): Promise<IFavourite[]> {
     return await this.#client
-      .insert(this.#client.schema.favourites)
+      .insert(favouritesTable)
       .values(input)
       .returning();
   }
 
-  async removeFavourite(userId: number, pizzaId: number): Promise<void> {
-    await this.#client
-      .delete(this.#client.schema.favourites)
-      .where((favs: typeof favouritesTable, { and, eq }: any) =>
-        and(eq(favs.userId, userId), eq(favs.pizzaId, pizzaId))
-      );
+ async removeFavourite(userId: number, pizzaId: number): Promise<void> {
+  await this.#client
+    .delete(favouritesTable)
+    .where(
+      and(
+        eq(favouritesTable.userId, String(userId)),
+        eq(favouritesTable.pizzaId, String(pizzaId))
+      )
+    );
   }
 }
 
