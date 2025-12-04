5c98a43dfe47b3e71947cd700436d7a3034846b3
Lasse Rasmussen <lass531n@stud.kea.dk>
2025-12-04 14:34:14 +0100

    added preload: false to userpage

--- apps/frontend/src/routes/userPage/index.tsx
+++ apps/frontend/src/routes/userPage/index.tsx
@@ -2,6 +2,7 @@ import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/userPage/')({
   component: UserPage,
+  preload: false,
 })
 
 function UserPage() {
