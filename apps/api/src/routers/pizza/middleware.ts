6cf3fa622d1662541ac47637d5d29336aaeb3078
Lasse-R <91476388+Lasse-R@users.noreply.github.com>
2025-12-13 21:34:40 +0100

    I no longer delete tables in beforeEach (#17)
    * I no longer delete tables in beforeEach
    
    * linted.... AGAIN

--- apps/api/src/routers/pizza/middleware.ts
+++ apps/api/src/routers/pizza/middleware.ts
@@ -1,12 +1,15 @@
 import { createMiddleware } from "hono/factory";
 import PizzaHandler from "$db/pizza/handler";
 import { IPizzaEnv } from "./publicRouter";
+import { getDB } from "$db/index"; // import getDB here
 
 const injectPizzaHandler = createMiddleware<IPizzaEnv>((c, next) => {
-  const pizzaHandler = new PizzaHandler(
-    c.env.DB_URL,
-    c.env.ENVIRONMENT !== "production",
-  );
+  // Create DB client once
+  const db = getDB(c.env.DB_URL, c.env.ENVIRONMENT !== "production");
+
+  // Pass DB to handler
+  const pizzaHandler = new PizzaHandler(db);
+
   c.set("pizzaHandler", pizzaHandler);
 
   return next();
