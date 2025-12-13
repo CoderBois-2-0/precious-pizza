77f1ef7429950517a3f002f31464f63cbcd3bb00
Lasse Rasmussen <lass531n@stud.kea.dk>
2025-12-13 13:22:36 +0100

    linted

--- apps/api/test/tests/pizzaHandler.test.ts
+++ apps/api/test/tests/pizzaHandler.test.ts
@@ -129,7 +129,7 @@ describe("PizzaHandler", {}, () => {
       "00000000-0000-0000-0000-000000000000", // non-existing ID
       {
         name: "Nope",
-      }
+      },
     );
 
     const rows = await db.select().from(pizzaTable).execute();
@@ -139,7 +139,7 @@ describe("PizzaHandler", {}, () => {
   // Invalid pizza deletion
   it("deleting a non-existing pizza does nothing", async () => {
     await expect(
-      handler.delete("00000000-0000-0000-0000-000000000000") // non-existing ID
+      handler.delete("00000000-0000-0000-0000-000000000000"), // non-existing ID
     ).resolves.not.toThrow();
   });
 
@@ -168,7 +168,7 @@ describe("PizzaHandler", {}, () => {
 
     // Expect the handler to reject the promise with a specific error
     await expect(handler.create(pizza)).rejects.toThrow(
-      "Name must be at least 1 character"
+      "Name must be at least 1 character",
     );
   });
 
