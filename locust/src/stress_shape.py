08bdc347850fc3d09353acf7b6965938882920f8
Doro-HD <76572055+Doro-HD@users.noreply.github.com>
2025-12-14 17:34:21 +0100

    Add locust (#21)

---
+++ locust/src/stress_shape.py
@@ -0,0 +1,16 @@
+from locust import LoadTestShape
+from util import create_stage
+
+
+class StressShape(LoadTestShape):
+    stages = [
+        create_stage(60, 100, 10),
+        create_stage(300, 1000, 10),
+    ]
+
+    def tick(self):
+        run_time = self.get_run_time()
+
+        for stage in self.stages:
+            if run_time < stage["duration"]:
+                return (stage["users"], stage["spawn_rate"])
